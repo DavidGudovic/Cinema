aae6cdbcbac74dea73ae5fbd6085779d66ff8dcd
David <dgudovic1@gmail.com>
2023-07-27 21:39:33 +0200

    deleted example tests

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -2,15 +2,10 @@
 
 namespace Tests\Unit\Models;
 
+use App\Models\User;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
-    {
-        $this->assertTrue(true);
-    }
+
 }
