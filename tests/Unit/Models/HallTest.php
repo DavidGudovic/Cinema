6663d77b317c4f5a32d9fceec7d7d77a88597e86
David <dgudovic1@gmail.com>
2023-07-27 20:12:47 +0200

    intial commit of model unit tests

---
+++ tests/Unit/Models/HallTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use PHPUnit\Framework\TestCase;
+
+class HallTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
