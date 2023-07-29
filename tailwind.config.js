ca321c1418300e43a9818472581bc86ee7645df4
David <dgudovic1@gmail.com>
2023-07-30 00:18:32 +0200

    added intersect alpine plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./resources/**/*.blade.php"],
+    theme: {
+        extend: {
+            //background image
+            backgroundImage: (theme) => ({
+                "business-pattern": "url('/images/biznis_showcase.png')",
+            }),
+        },
+    },
+    plugins: [],
+};
