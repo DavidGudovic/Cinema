b7046eb3b61a8652350ab9f817e2a334b3ac2cb7
David <dgudovic1@gmail.com>
2023-07-31 00:19:28 +0200

    removed last_name

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('username')->unique();
-            $table->string('role');
+            $table->enum('role', ['ADMIN', 'CLIENT', 'BUSINESS_CLIENT', 'MANAGER'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
