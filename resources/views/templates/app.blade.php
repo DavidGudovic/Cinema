e6f0c71e280587d2a52e2095c9eabc8557fbf422
David <dgudovic1@gmail.com>
2023-08-04 13:10:24 +0200

    frontend changes

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -1,42 +1,44 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-  <meta charset="utf-8">
-  <meta name="author" content="David Gudović">
-  <meta name="description" content="Online bioskop">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="csrf-token" content="{{csrf_token()}}">
+    <meta charset="utf-8">
+    <meta name="author" content="David Gudović">
+    <meta name="description" content="Online bioskop">
+    <meta name="viewport" content="width=device-width, initial-scale=1, max-width=1920px">
+    <meta name="csrf-token" content="{{csrf_token()}}">
 
-  <title>Biskop Cinemanija</title>
-  <link rel="icon" href="{{URL('images/logo_cut-min.svg')}}">
-  @vite('resources/js/app.js')
-  @livewireStyles
+    <title>Biskop Cinemanija</title>
+    <link rel="icon" href="{{URL('images/logo_cut-min.svg')}}">
+    @vite('resources/js/app.js')
+    @livewireStyles
 </head>
-   <body class="flex flex-col antialiased min-h-screen bg-gray-950 text-white">
-     <!--HEADER OPTIONAL-->
-     @yield('header')
-     <!--END HEADER-->
-     <!--NAVBAR-->
-     @include('includes.navbar')
-     <!--END NAVBAR -->
+<body class="flex flex-col antialiased min-h-screen bg-gray-950 text-white">
+    <!--HEADER OPTIONAL-->
+    @yield('header')
+    <!--END HEADER-->
+    <!--NAVBAR-->
+    @include('includes.navbar')
+    <!--END NAVBAR -->
 
-     <!-- CONTENT - Injectable background-->
-     <div class="flex flex-row flex-1 justify-center @yield('background-pattern')">
-       <!-- MAIN CONTENT -->
-       <main class="w-screen">
-         @yield('content')
-       </main>
-       <!-- END MAIN CONTENT -->
-     </div>
-     <!-- END CONTENT -->
+    <!-- CONTENT - Injectable background-->
+    <div class="flex flex-row flex-1 justify-center @yield('background-pattern')">
+        <!-- MAIN CONTENT -->
+        <main class="w-screen">
+            @yield('content')
+        </main>
+        <!-- END MAIN CONTENT -->
+    </div>
+    <!-- END CONTENT -->
 
-     <!--FOOTER-->
-     @include('includes.footer')
-     <!--END FOOTER-->
+    <!--FOOTER-->
+    @include('includes.footer')
+    <!--END FOOTER-->
 
-     <!-- GLOBAL MODALS -->
-     <!-- END MODALS -->
-     @livewireScripts
-     @yield('scripts')
-   </body>
+    <!-- GLOBAL MODALS -->
+    @yield('sidebar')
+    @yield('modal')
+    <!-- END MODALS -->
+    @livewireScripts
+    @yield('scripts')
+</body>
 </html>
