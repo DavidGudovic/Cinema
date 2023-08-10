949be1c135654034bc29100a8f0f81ba987c458b
David <dgudovic1@gmail.com>
2023-08-11 01:16:52 +0200

    locale hotfix 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,18 +7,24 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     */
+    * Register any application services.
+    */
     public function register(): void
     {
         //
     }
 
     /**
-     * Bootstrap any application services.
-     */
+    * Bootstrap any application services.
+    */
     public function boot(): void
     {
-        //
+
+        if (app()->environment('production')) {
+            setlocale(LC_TIME, 'sr_RS@latin');
+        } else {
+            setlocale(LC_TIME, 'sr_Latn_RS.UTF-8');
+        }
+
     }
 }
