1bccfb11ecbc347c67df4583818be05421bb1f24
David <dgudovic1@gmail.com>
2023-08-13 23:29:22 +0200

    attempting fix on server

--- app/Mail/VerifyEmail.php
+++ app/Mail/VerifyEmail.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class VerifyEmail extends Mailable implements ShouldQueue
+class VerifyEmail extends Mailable
 {
     use Queueable, SerializesModels;
     private $link;
