459a9426e27032d03d65031ce00a1e23edb8b42f
David <dgudovic1@gmail.com>
2023-08-23 23:07:08 +0200

    changed multipart form to livewire

--- resources/views/admin/screening/create.blade.php
+++ resources/views/admin/screening/create.blade.php
@@ -1,85 +1,11 @@
 @extends('templates.administration')
 
 @section('content')
-    <!-- Container -->
-    <div x-data="{step: 1}" class="relative flex justify-center items-center gap-6 w-full h-screen overflow-hidden">
-        <!-- Heading -->
-        <div class="absolute top-6 md:top-20 flex flex-col justify-center items-center">
-            <p x-transition:enter="transition ease-out-in duration-700 transform"
-               x-transition:enter-start="-translate-y-[150%]"
-               x-transition:enter-end="translate-y-0" x-cloak x-show="step == 1" class="h-full text-2xl font-bold">
-                Izaberite salu</p>
-            <p x-transition:enter="transition ease-out-in duration-700 transform"
-               x-transition:enter-start="-translate-y-[150%]"
-               x-transition:enter-end="translate-y-0" x-cloak x-show="step == 2" class="h-full text-2xl font-bold">
-                Izaberite tehnologiju</p>
-            <p x-transition:enter="transition ease-out-in duration-700 transform"
-               x-transition:enter-start="-translate-y-[150%]"
-               x-transition:enter-end="translate-y-0" x-cloak x-show="step == 3" class="h-full text-2xl font-bold">
-                Izaberite vreme</p>
-        </div>
-
-        <!-- Steps -->
-        <div class="flex flex-col flex-1 h-full">
-            <!-- Halls -->
-            <x-step>
-                <x-slot:step>1</x-slot:step>
-                <div class="grid grid-cols-1 md:grid-cols-2 justify-center mt-44 md:mt-12 gap-6 md:gap-12">
-                    @foreach($halls as $hall)
-                        <div class="flex flex-col justify-center items-center gap-2">
-                            <img x-on:click="step++" src="{{URL('images/halls/' . $hall->image_url)}}"
-                                 class="w-40 md:w-80" alt="hall {{$hall->name}}">
-                            <p class="text-center">{{$hall->name}}</p>
-                        </div>
-                    @endforeach
-                </div>
-            </x-step>
-            <!-- End halls-->
-
-            <!-- Tags -->
-            <x-step>
-                <x-slot:step>2</x-slot:step>
-                <div class="flex justify-center gap-12">
-                    <div class="flex flex-col md:flex-row justify-center items-center gap-6">
-                        <img src="{{URL('images/tags/dolby.png')}}" alt="tag dolby atmos" class="w-24 md:w-32">
-                        <p class="font-bold text-xl mx-8"> + </p>
-                        @foreach($tags as $tag)
-                            @if($tag->name !== 'Dolby Atmos')
-                                <img x-on:click="step++" src="{{URL('images/tags/' . $tag->image_url)}}"
-                                     class="cursor-pointer w-24 md:w-32" alt="tag {{$tag->name}}">
-                            @endif
-                        @endforeach
-                    </div>
-                </div>
-            </x-step>
-            <!-- End tags-->
-
-            <!-- Date -->
-            <x-step>
-                <x-slot:step>3</x-slot:step>
-                <div>
-                    <div class="flex flex-col justify-center items-center gap-2">
-                        <p x-on:click="step++" class="text-center">Select date</p>
-                    </div>
-                </div>
-            </x-step>
-            <!-- End date-->
-
-
-            <!-- Success message -->
-            <x-step>
-                <x-slot:step>4</x-slot:step>
-                <a href="{{route('screenings.create', ['movie' => $movie])}}"
-                   class="text-center font-extrabold text-5xl cursor-pointer">Finish</a>
-            </x-step>
-            <!-- End success message-->
-        </div>
-        <!-- End steps -->
-
-        <!-- Breadcrumbs -->
-        <x-breadcrumbs class="absolute right-6 md:right-12"/>
-        <!-- End breadcrumbs -->
-
-    </div>
-    <!-- End container -->
+    <!-- Multistep form -->
+    @livewire('admin.screening.create', [
+	 'movie' => $movie,
+	 'halls' => $halls,
+	 'tags' => $tags,
+ ])
+    <!-- End multistep from -->
 @endsection
--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -1,26 +1,19 @@
 <div {{ $attributes->merge(['class' => 'flex flex-col items-center']) }}>
-	<!-- Step 1 -->
-	<div x-on:click="step = 1" x-bind:class="step == 1 ? 'border-green-500' : (step > 1 ? 'cursor-pointer bg-green-500' : 'bg-transparent')"
-	     class="flex justify-center items-center w-8 h-8 md:w-10 md:h-10 rounded-full border">
-		<p class="text-xs md:text-base" x-html="step > 1 ? '&#10004;' : '1'"></p>
-	</div>
-	<!-- End step 1 -->
+    <template x-for="i in totalSteps" :key="i">
+        <!-- Container for Step and Line -->
+        <div>
+            <!-- Step -->
+            <div x-on:click="if (step > i) window.livewire.emit('backtrack', i)"
+                 x-bind:class="step == i ? 'border-green-500' : (step > i ? 'cursor-pointer bg-green-500' : 'bg-transparent')"
+                 class="flex justify-center items-center w-8 h-8 md:w-10 md:h-10 rounded-full border">
+                <p class="text-xs md:text-base" x-html="step > i ? '&#10004;' : i"></p>
+            </div>
+            <!-- End Step -->
 
-	<div x-bind:class="step > 1 ? 'bg-green-500' : 'bg-white' " class="w-[0.05rem] h-28"></div> <!-- Line -->
-
-	<!-- Step 2-->
-	<div x-on:click="step = step > 2 ? 2 : step" x-bind:class="step == 2 ? 'border-green-500' : (step > 2 ? 'cursor-pointer bg-green-500' : 'bg-transparent' ) "
-	     class="flex justify-center items-center w-8 h-8 md:w-10 md:h-10 rounded-full border">
-		<p class="text-xs md:text-base" x-html="step > 2 ? '&#10004;' : '2'"></p>
-	</div>
-	<!-- End step-2 -->
-
-	<div x-bind:class="step > 2 ? 'bg-green-500' : 'bg-white' " class="w-[0.05rem] h-28"></div> <!-- Line -->
-
-	<!-- Step 3 -->
-	<div x-bind:class="step == 3 ? 'border-green-500' : (step > 3 ? 'bg-green-500' : 'bg-transparent') "
-	     class="flex justify-center items-center w-8 h-8 md:w-10 md:h-10 rounded-full border">
-		<p class="text-xs md:text-base" x-html="step > 3 ? '&#10004;' : '3'"></p>
-	</div>
-	<!-- End step 3 -->
+            <!-- Line (except for the last step) -->
+            <template x-if="i != totalSteps">
+                <div x-bind:class="step > i ? 'bg-green-500' : 'bg-white'" class="m-auto w-[0.05rem] h-28"></div>
+            </template>
+        </div>
+    </template>
 </div>
