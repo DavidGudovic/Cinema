1d65b7cf412cb9aeccf98a0a7ca7d9e4ffb9ae5d
David <dgudovic1@gmail.com>
2023-08-25 21:23:05 +0200

    added default reject message

---
+++ config/messages.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'request_cancelled_intersect' => 'Nažalost vaš zahtev je otkazan jer se preklapa sa našim novim rasporedom projekcija, molimo Vas da napravite novi zahtev.',
+];
