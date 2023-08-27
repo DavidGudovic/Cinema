aa62ac8af9b4cc7f453982282e2b33db431b94ba
David <dgudovic1@gmail.com>
2023-08-27 13:09:48 +0200

    refactored to use traits

--- app/Http/Livewire/Charts/Advert.php
+++ app/Http/Livewire/Charts/Advert.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Livewire\Charts;
 
+use App\Traits\ChartModelBuilder;
 use Livewire\Component;
 use App\Services\AdvertService;
-use Asantibanez\LivewireCharts\Facades\LivewireCharts;
 use App\Models\Advert as AdvertModel;
 
 class Advert extends Component
 {
+    use ChartModelBuilder;
+
     public $advert;
     public $request;
     protected $lineChartModel;
@@ -29,39 +31,8 @@ class Advert extends Component
         'refreshChart' => 'refreshChart',
     ];
 
-    public function mount(){
-        $this->refreshChart(new AdvertService(), $this->advert);
-    }
-    /**
-    *  Livewire refuses to rerender the charts without this method, something about reactiveKeys, no documentation
-    */
-    public function refreshChart(AdvertService $advertService,AdvertModel $advert)
-    {
-        $this->advert = $advert;
-        $this->lineChartModel = LivewireCharts::lineChartModel()
-        ->setTitle('Pregledi po danima')
-        ->setAnimated(false)
-        ->setSmoothCurve()
-        ->withGrid()
-        ->setColors($this->colors);
-
-        $this->pieChartModel = LivewireCharts::pieChartModel()
-        ->setTitle('Procenat pregledanih')
-        ->setAnimated(true)
-        ->setType('pie')
-        ->legendPositionBottom()
-        ->legendHorizontallyAlignedCenter();
-
-        foreach($advertService->getViewsByWeekMap($this->advert) as $date => $count) {
-            $this->lineChartModel->addPoint($date, $count);
-        }
-
-        $adverts_scheduled = $advertService->getScheduledCount($this->advert);
-        $adverts_shown = $this->advert->quantity - $this->advert->quantity_remaining - $adverts_scheduled;
-        $this->pieChartModel
-        ->addSlice('Pregledano', $adverts_shown, $this->colors['green'])
-        ->addSlice('Zakazano', $adverts_scheduled , $this->colors['indigo'])
-        ->addSlice('Nepregledano', $this->advert->quantity_remaining, $this->colors['red']);
+    public function mount(AdvertService $advertService){
+        $this->refreshChart($advertService, $this->advert);
     }
 
     public function render()
@@ -70,6 +41,41 @@ class Advert extends Component
             'lineChartModel' => $this->lineChartModel,
             'pieChartModel' => $this->pieChartModel,
         ]);
+    }
+
+    /**
+     *  Fix for reactivity with nested livewire components, whenever the parent component changes it emits refreshChart, handled here
+     */
+    public function refreshChart(AdvertService $advertService,AdvertModel $advert): void
+    {
+        $this->advert = $advert;
+
+        $this->lineChartModel = $this->buildChartModel(
+            title: 'Pregledi po danima',
+            data: $advertService->getViewsByWeekMap($this->advert),
+            colors: $this->colors,
+            chart_type: 'line',
+        );
+
+        $this->pieChartModel = $this->buildChartModel(
+            title: 'Procenat pregledanih',
+            data: $this->getPieData($advertService),
+            colors: ['Pregledano' => $this->colors['green'], 'Zakazano' => $this->colors['indigo'], 'Nepregledano' => $this->colors['red']],
+            chart_type: 'pie',
+            animated: true,
+        );
+    }
+
+    public function getPieData(AdvertService $advertService) : array
+    {
+        $adverts_scheduled = $advertService->getScheduledCount($this->advert);
+        $adverts_shown = $this->advert->quantity - $this->advert->quantity_remaining - $adverts_scheduled;
+
+        return [
+            'Pregledano' => $adverts_shown,
+            'Zakazano' => $adverts_scheduled,
+            'Nepregledano' => $this->advert->quantity_remaining,
+        ];
     }
 
 }
