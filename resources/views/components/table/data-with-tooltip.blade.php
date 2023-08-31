0d2be9187c20c9e98dbd458f9cc796148ee00d17
David <dgudovic1@gmail.com>
2023-08-31 19:43:48 +0200

    styling

--- resources/views/components/table/data-with-tooltip.blade.php
+++ resources/views/components/table/data-with-tooltip.blade.php
@@ -2,8 +2,8 @@
 
 <td x-on:mouseenter="visibleToolTip = {{$model}}, toolTipIndex = {{$toolTipIndex}}"
     x-on:mouseleave="visibleToolTip = false"
-    {{ $attributes->merge(['class' => 'group m-2 line-clamp-2']) }}>
+    {{ $attributes->merge(['class' => 'group m-2 line-clamp-2 ']) }}>
     {{ implode(' ',explode(' ', $text, 3))}}
     <span x-cloak x-show="visibleToolTip == {{$model}} && toolTipIndex == {{$toolTipIndex}}"
-          class=" transition-opacity bg-gray-800 text-gray-100 p-2 text-sm rounded-md  absolute {{$position}} z-20 w-96 h-auto">{{$text}}</span>
+          class=" transition-opacity bg-gray-800 text-gray-100 p-2 text-sm rounded-md  absolute {{$position}} z-50 w-96 h-auto">{{$text}}</span>
 </td>
